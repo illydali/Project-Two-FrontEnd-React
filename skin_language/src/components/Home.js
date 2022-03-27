55bd0f226ba1aa6ac1637b79a1019a18591c1fad
illydali <ili.zaynal@gmail.com>
2022-03-27 19:56:19 +0000

    added comments for hero image

--- skin_language/src/components/Home.js
+++ skin_language/src/components/Home.js
@@ -7,7 +7,8 @@ export default function Home() {
         <Typography variant='h3' component='h1'>
             Hello
             <Box marginTop={3}
-                sx={{ display: 'flex', display: {xs:'none' } }}>
+                // hide hero image for now when in xs and sm size
+                sx={{ display: 'flex', display: {xs:'none', sm:'none' } }}>
                 <img src='https://media.istockphoto.com/photos/smiling-brunette-woman-holding-two-slice-of-lemon-in-front-of-her-picture-id912862714?b=1&k=20&m=912862714&s=170667a&w=0&h=0KmCyEKYqN41soTntdEQeThTcUuh0OaBXxlEVKZHDt0='
                     alt=''
                     height={450} 
